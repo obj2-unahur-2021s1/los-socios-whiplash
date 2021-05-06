2fb9d0576edca127a7af15d2c44326e8b1b587fe
msunh <msalto.unh@gmail.com>
2021-05-06 16:55:15 -0300

    intentando entender patrones/resolucion

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,69 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+
+
+class Cliente(val plataEnElBolsillo:Int) : EstadoDeAnimo, Barrio {
+    //val interfazAnimo = EstadoDeAnimo
+
+    fun cuantaPropinaDejaria(costoPedido : Int) {
+        //EstadoDeAnimo.accionQueRealiza()
+    }
+
 }
+
+
+interface EstadoDeAnimo {
+    fun accionQueRealiza()
+}
+
+object Enojado : EstadoDeAnimo {
+    override fun accionQueRealiza() = println ("no deja nada")
+}
+
+object EstaFeliz : EstadoDeAnimo{
+    override fun accionQueRealiza() {
+
+    }
+}
+object Indiferente : EstadoDeAnimo{
+    override fun accionQueRealiza() {
+
+    }
+}
+object Resfriado : EstadoDeAnimo{
+    override fun accionQueRealiza() {
+
+    }
+}
+
+
+interface Barrio {
+    abstract fun efectoSobreLaPropina()
+}
+
+object LasRosas : Barrio{
+    override fun efectoSobreLaPropina() {
+        TODO("Not yet implemented")
+    }
+}
+
+object LasLauchas : Barrio{
+    override fun efectoSobreLaPropina() {
+        TODO("Not yet implemented")
+    }
+}
+
+object BarrioVerde : Barrio{
+    override fun efectoSobreLaPropina() {
+        TODO("Not yet implemented")
+    }
+}
+
+object LasTorres : Barrio{
+    override fun efectoSobreLaPropina() {
+        TODO("Not yet implemented")
+    }
+}
+
+
+
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,8 +1,25 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
+  val lasRosas = LasRosas
+  val lasLauchas = LasLauchas
+  val enojado = Enojado
+  val resfriado = Resfriado
+
+  val clienteEnojado = Cliente(enojado,200,lasRosas)
+  val clienteResfriado = Cliente(resfriado,1000,lasLauchas)
+  //clienteEnojado.cuantaPropinaDejaria(1500)
+
   describe("Un/a cliente") {
+
+    it("cuanta plata deja un cliente resfriado"){
+    clienteResfriado.cuantaPropinaDejaria(1000).shouldBe(500)
+
+
+    }
+
   }
 })
